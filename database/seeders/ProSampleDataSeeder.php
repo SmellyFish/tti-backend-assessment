3f28613d71bae1a62ea393a700d14ca8fc915dff
Nikita Makeyev <nikita@nikitamakeyev.com>
2026-04-20 14:25:42 -0700

    Update seed data to include multiple submissions per instrument

--- database/seeders/ProSampleDataSeeder.php
+++ database/seeders/ProSampleDataSeeder.php
@@ -9,13 +9,18 @@ use App\Models\Patient;
 use App\Models\Question;
 use App\Models\Submission;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class ProSampleDataSeeder extends Seeder
 {
     private const PATIENT_COUNT = 50;
+
     private const INSTRUMENT_COUNT = 60;
+
     private const MIN_QUESTIONS_PER_INSTRUMENT = 3;
+
     private const MAX_QUESTIONS_PER_INSTRUMENT = 6;
+
     private const SUBMISSION_COUNT = 240;
 
     public function run(): void
@@ -32,9 +37,9 @@ class ProSampleDataSeeder extends Seeder
     }
 
     /**
-     * @return \Illuminate\Support\Collection<int, Patient>
+     * @return Collection<int, Patient>
      */
-    private function seedPatients(): \Illuminate\Support\Collection
+    private function seedPatients(): Collection
     {
         $patients = collect();
 
@@ -50,9 +55,9 @@ class ProSampleDataSeeder extends Seeder
     }
 
     /**
-     * @return \Illuminate\Support\Collection<int, Instrument>
+     * @return Collection<int, Instrument>
      */
-    private function seedInstrumentsWithQuestions(): \Illuminate\Support\Collection
+    private function seedInstrumentsWithQuestions(): Collection
     {
         $instruments = collect();
 
@@ -92,37 +97,67 @@ class ProSampleDataSeeder extends Seeder
     }
 
     /**
-     * @param  \Illuminate\Support\Collection<int, Patient>  $patients
-     * @param  \Illuminate\Support\Collection<int, Instrument>  $instruments
+     * @param  Collection<int, Patient>  $patients
+     * @param  Collection<int, Instrument>  $instruments
      */
     private function seedSubmissions(
-        \Illuminate\Support\Collection $patients,
-        \Illuminate\Support\Collection $instruments
+        Collection $patients,
+        Collection $instruments
     ): void {
-        for ($i = 0; $i < self::SUBMISSION_COUNT; $i++) {
+        $randomCount = self::SUBMISSION_COUNT - (self::PATIENT_COUNT * 2);
+
+        foreach ($patients as $patient) {
+            /** @var Instrument $chosen */
+            $chosen = $instruments->random();
+
+            $this->createSubmissionWithAnswers(
+                $patient,
+                $chosen,
+                fake()->dateTimeBetween('-150 days', '-30 days')
+            );
+            $this->createSubmissionWithAnswers(
+                $patient,
+                $chosen,
+                fake()->dateTimeBetween('-29 days', 'now')
+            );
+        }
+
+        for ($i = 0; $i < $randomCount; $i++) {
             /** @var Patient $patient */
             $patient = $patients->random();
             /** @var Instrument $instrument */
             $instrument = $instruments->random();
 
-            $submission = Submission::query()->create([
-                'patient_id' => $patient->id,
-                'instrument_id' => $instrument->id,
-                'submitted_at' => fake()->dateTimeBetween('-180 days', 'now'),
+            $this->createSubmissionWithAnswers(
+                $patient,
+                $instrument,
+                fake()->dateTimeBetween('-180 days', 'now')
+            );
+        }
+    }
+
+    private function createSubmissionWithAnswers(
+        Patient $patient,
+        Instrument $instrument,
+        \DateTimeInterface $submittedAt
+    ): void {
+        $submission = Submission::query()->create([
+            'patient_id' => $patient->id,
+            'instrument_id' => $instrument->id,
+            'submitted_at' => $submittedAt,
+        ]);
+
+        foreach ($instrument->questions as $question) {
+            $value = match ($question->response_type) {
+                ResponseType::Scale1To5 => fake()->numberBetween(1, 5),
+                ResponseType::YesNo => fake()->boolean(),
+                ResponseType::FreeText => fake()->boolean(20) ? '' : fake()->sentence(fake()->numberBetween(4, 12)),
+            };
+
+            $submission->answers()->create([
+                'question_id' => $question->id,
+                'value' => $value,
             ]);
-
-            foreach ($instrument->questions as $question) {
-                $value = match ($question->response_type) {
-                    ResponseType::Scale1To5 => fake()->numberBetween(1, 5),
-                    ResponseType::YesNo => fake()->boolean(),
-                    ResponseType::FreeText => fake()->boolean(20) ? '' : fake()->sentence(fake()->numberBetween(4, 12)),
-                };
-
-                $submission->answers()->create([
-                    'question_id' => $question->id,
-                    'value' => $value,
-                ]);
-            }
         }
     }
 }
